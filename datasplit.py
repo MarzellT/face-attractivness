c4775b5ac007d6b0cd82c1bbfa5c77e391e4c294
Tobias Marzell <tobias.marzell@gmail.com>
2019-07-23 03:52:44 +0200

    updated datasplit.py and .gitignore

--- datasplit.py
+++ datasplit.py
@@ -3,9 +3,12 @@ import os
 import random
 from shutil import copyfile
 
-files = sys.argv[1:]
-for i in range(0,len(files),3):
-    choice = random.choice(os.listdir(files[i]))
-    file = './'+files[i]+'/'+choice
-    print(file)
-    copyfile(file, 'data/raten/' + str(i) + choice)
+choices = []
+while len(choices)<int(sys.argv[1])-1:
+    choice = random.choice(os.listdir(sys.argv[2]))
+    if not (choice in choices):
+        choices.append(choice)
+        choice2 = random.choice(os.listdir(sys.argv[2]+'/'+choice))
+        file = sys.argv[2]+choice+'/'+choice2
+        print(file)
+        copyfile(file, 'data/raten/' + choice+choice2)
