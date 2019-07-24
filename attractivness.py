a01c08459a585721c5e5421dc58e9d4270b774ae
Tobias Marzell <tobias.marzell@gmail.com>
2019-07-24 15:01:15 +0200

    training is now possible

--- attractivness.py
+++ attractivness.py
@@ -11,6 +11,7 @@ from keras_vggface.utils import preprocess_input
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 import keras.backend as K
+import numpy as np
 import pandas as pd
 import mtcnn
 from PIL import Image
@@ -42,7 +43,6 @@ def prepare_frame(files):
         for j in range(len(dirs)):
             dirs[j] = PureWindowsPath(dirs[j][:i-1])
             dirs[j] = Path(dirs[j])
-            print(dirs[j])
             files[j] = files[j][i:]
             imagefile = os.path.join(dirs[j], files[j])
             image_already_in_list = False
@@ -67,7 +67,7 @@ def get_avg_ratings(ratingslist):
         for rating in ratingslist[j]:
             avg += rating
             i += 1
-        ratingslist[j] = avg/i
+        ratingslist[j] = avg/i/10
 
     return ratingslist
 
@@ -92,7 +92,7 @@ def prepare_image(file):
         raise ValueError
     return prepared[0]
 
-def create_model(neurons=224, activation='relu'):
+def create_model(neurons=224, activation='sigmoid'):
     """ Create the transfer learning model. """
     base_model = VGGFace(include_top=False, input_shape=(224, 224, 3))
     x = base_model.output
@@ -128,8 +128,40 @@ def train_model(model, data, epochs=20, batch_size=32, checkpoint=None):
         model.fit(images, targets, validation_split=0.25, epochs=epochs, batch_size=batch_size, shuffle=True)
     return model
 
+def load_model(file):
+    """ Load a model from json file. """
+    json_file = open("file", 'r')
+    model = json_file.read()
+    json_file.close()
+    model = model_from_json(model)
+
+    return model
+    
+def save_model(model, file):
+    """ Save a model to json file. """
+    with open(file, 'w') as f:
+        f.write(model.to_json())
+
+def test_on_batch(model, files):
+    """ Test model on files in dir or pass through list. 
+    
+    If you want to change color_space more changes to the code have to be made.
+    """
+    batch = []
+    if isinstance(files, str):
+        files = get_files_in_dir(files)
+    for file in files:
+        batch.append(prepare_image(file))
+    batch = np.array(batch)
+    preds = model.predict_on_batch(batch)
+
+    return preds
+
 def main():
     files = sys.argv[1:]
+    train = True
+    name = "first_try"
+
     model = create_model()
     for layer in model.layers:
         layer.trainable = False
@@ -138,7 +170,17 @@ def main():
         layer.trainable = True
     
     data = create_dataframe(files)
-    print(data)
+
+    if train:
+        model = train_model(model, data, checkpoint=name, epochs=5, batch_size=20)
+        model.save_weights('weights/' + name + '.h5')
+    else:
+        model.load_weights("first_try.h5")
+
+    predictions = test_on_batch(model, data.iloc[:,0])
+    
+    for i in range(len(predictions)):
+        print(data.iloc[i,0], predictions[i]*10)
 
 if __name__ == "__main__":
     main()
